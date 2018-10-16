8b96bdb9f9cdf7e6781b453f5e5f64da46ac1163
FunkAdri <adrienhenry.fp@gmail.com>
2018-10-16 10:01:30 +0200

    fin exo2

---
+++ exo2/jq.js
@@ -0,0 +1,17 @@
+var count = 0;// declaration de la variable count
+
+$(function() { // permet de lancer les fonctions une fois le DOM chargé
+
+  $("#buttonp").click(function() { // fonction incrementer variable count
+    count++;
+    console.log("var :"+count);// vérification de la variable count dans la console
+    $("#text").val("Nombre de click : "+count);
+  });
+
+  $("#buttonm").click(function() { // même chose
+    count--;
+    console.log("var :"+count);
+    $("#text").val("Nombre de click : "+count);
+  });
+
+});
